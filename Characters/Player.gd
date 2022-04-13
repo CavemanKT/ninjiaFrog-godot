458dc10d202579a2ba8476e5c1b2af8cacee814a
kttechcelerate <kt@techcelerate.org>
2022-04-13 13:43:30 +0800

    added debug label

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -2,24 +2,41 @@ extends KinematicBody2D
 
 export(float) var move_speed = 200
 export(float) var jump_impulse = 600
-
+export(int) var max_jumps = 2
 enum STATE {IDLE, RUN, JUMP, DOUBLE_JUMP, FALL, WALL_JUMP, HIT}
+
+onready var animated_sprite = $AnimatedSprite
+onready var animation_tree = $AnimationTree
+
+signal changed_state(new_state_str, new_state_id)
 
 var velocity : Vector2
 
-var current_state = STATE.IDLE setget set_current_state
+var current_state = STATE.IDLE setget set_current_state # does this mean every time current_state is assigned a new value, set_current_state has to run itself.
+
 var jumps = 0
 
 func _physics_process(delta):
 	var input = get_player_input()
+	adjust_flip_direction(input)
 	velocity = Vector2(
 		input.x * move_speed,
-		min(velocity.y + GameSettings.gravity, GameSettings.terminal_velocity)  # no matter how long it goes, it never goes faster than the terminal_velocity
+		min(velocity.y + GameSettings.gravity, GameSettings.terminal_velocity)  
 	)
 	
 	velocity = move_and_slide(velocity, Vector2.UP)    # after the collision, the the set of velocity will be zero.
-	
+	set_anim_parameters()
 	pick_next_state()
+
+func adjust_flip_direction(input: Vector2):
+	if(sign(input.x) == 1):
+		animated_sprite.flip_h = false
+	elif(sign(input.x) == -1):
+		animated_sprite.flip_h = true
+
+func set_anim_parameters():
+	animation_tree.set("parameters/x_sign/blend_position", sign(velocity.x))
+	animation_tree.set("parameters/y_sign/blend_amount", sign(velocity.y))
 
 func pick_next_state():
 	if(is_on_floor()):
@@ -31,8 +48,9 @@ func pick_next_state():
 		else:
 			self.current_state = STATE.IDLE
 	else:
-		
-		pass
+		if(Input.is_action_just_pressed('jump') && jumps < max_jumps):
+			self.current_state = STATE.DOUBLE_JUMP
+
 
 	
 func get_player_input():
@@ -47,13 +65,16 @@ func jump():
 	velocity.y = -jump_impulse
 	jumps += 1
 	
-	
 # SETTERS
 func set_current_state(new_state):
 	match(new_state):
 		STATE.JUMP:
 			jump()
-		
+		STATE.DOUBLE_JUMP:
+			jump()
+			animation_tree.set('parameters/double_jump/active', true)
 		
 	current_state = new_state
+	emit_signal("changed_state", STATE.keys()[new_state], new_state)
 	
+
